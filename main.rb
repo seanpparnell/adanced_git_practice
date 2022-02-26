dc313ebc2aeba61a7511569487838bd45278f2a3
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2022-02-26 12:05:30 -0700

    colorize menu

---
+++ main.rb
@@ -0,0 +1,27 @@
+require 'colorize'
+require_relative 'git'
+
+class Main
+  include Git
+
+  def self.menu
+    puts 'MAIN MENU'.colorize(:red)
+    puts '1: Enter git command'.colorize(:cyan)
+    puts '2: Exit'.colorize(:cyan)
+    choice = gets.to_i
+    case choice
+    when 1
+      puts 'Enter git command'.colorize(:green)
+      Git.puts_git(gets.strip)
+    when 2
+      exit
+    else
+      puts 'Invalid choice'.colorize(:red)
+    end
+
+    menu
+  end
+
+end
+
+Main.menu
